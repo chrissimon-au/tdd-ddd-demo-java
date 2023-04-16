7b5877b38f0ef7490b5599df2029f9f916d9627b
Chris Simon <chris@devcycles.io>
2023-04-16 02:48:39 +0000

    Initialize base maven project using spring initializr.  See init.sh script.

---
+++ src/test/java/au/chrissimon/universityapi/UniversityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package au.chrissimon.universityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
