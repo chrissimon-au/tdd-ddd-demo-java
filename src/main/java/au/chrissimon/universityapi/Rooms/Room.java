53da4deb8307b549246e8889dde98bd05747e7f9
Chris Simon <chris@devcycles.io>
2023-04-17 06:38:27 +0000

    12-GREEN: Change factory and Room entity to ensure details are returned on setup.

--- src/main/java/au/chrissimon/universityapi/Rooms/Room.java
+++ src/main/java/au/chrissimon/universityapi/Rooms/Room.java
@@ -4,10 +4,14 @@ import java.util.UUID;
 
 public class Room {
     private UUID id;
-
-    public Room(UUID id) {
+    private String name;
+    private int capacity;
+    
+    public Room(UUID id, String name, int capacity) {
         super();
         setId(id);
+        setName(name);
+        setCapacity(capacity);
     }
 
     public UUID getId() {
@@ -18,7 +22,23 @@ public class Room {
         this.id = id;
     }
 
-    public static Room setupNew() {
-        return new Room(UUID.randomUUID());
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public static Room setupNew(Room roomRequest) {
+        return new Room(UUID.randomUUID(), roomRequest.getName(), roomRequest.getCapacity());
     }
 }
