253570f835372e21cf790d58310c3bde45e07715
Chris Simon <chris@devcycles.io>
2023-04-17 07:18:12 +0000

    14-REFACTOR: parameterise checking room details tests in prep for storing and retrieving multiple tests.

--- src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
+++ src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
@@ -2,8 +2,14 @@ package au.chrissimon.universityapi.Rooms;
 
 import java.net.URI;
 import java.util.UUID;
+import java.util.stream.Stream;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -52,10 +58,17 @@ public class RoomTests {
         assertThat(newRoom.getCapacity()).isEqualTo(roomRequest.getCapacity());
 	}
 
-    @Test
-    public void givenIHaveSetupARoom_WhenICheckItsDetails()
+    private static Stream<Arguments> testRooms() {
+        return Stream.of(
+            arguments("Test Room", 5)
+        );
+    }
+
+    @ParameterizedTest
+	@MethodSource("testRooms")
+    public void givenIHaveSetupARoom_WhenICheckItsDetails(String name, int capacity)
     {
-        SetupRoomRequest roomRequest = new SetupRoomRequest("Test Room", 5);
+        SetupRoomRequest roomRequest = new SetupRoomRequest(name, capacity);
 
         URI newRoomLocation = roomApi.setupRoom(roomRequest)
 				.expectBody(RoomResponse.class)
