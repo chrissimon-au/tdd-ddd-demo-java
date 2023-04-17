96e16c7930f44c2e007dd2f5cccedd9367b1bdb1
Chris Simon <chris@devcycles.io>
2023-04-17 06:13:24 +0000

    10-RED: Update test to assert that new id is allocated for newly setup rooms.

--- src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
+++ src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
@@ -1,10 +1,14 @@
 package au.chrissimon.universityapi.Rooms;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RoomTests {
@@ -17,11 +21,21 @@ public class RoomTests {
 		ResponseSpec response = roomApi.setupRoom();
 
         itShouldSetupANewRoom(response);
+        itShouldAllocateANewId(response);
 	}
 
     private void itShouldSetupANewRoom(ResponseSpec response) {
         response
             .expectStatus()
             .isCreated();
+    }
+
+    private void itShouldAllocateANewId(ResponseSpec response) {
+        response
+			.expectBody(RoomResponse.class)
+				.value(room -> {
+					assertThat(room.getId()).isNotEqualTo(new UUID(0, 0));
+                    assertThat(room.getId()).isNotNull();
+				});
     }
 }
