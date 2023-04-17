aebf4b16574a4e067cb47e7d4c0a4ec13eabc35f
Chris Simon <chris@devcycles.io>
2023-04-17 20:36:34 +0000

    16-GREEN: add simple implementation for missing room.

--- src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
+++ src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
@@ -3,12 +3,14 @@ package au.chrissimon.universityapi.Rooms;
 import java.net.URI;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
@@ -41,7 +43,7 @@ public class RoomController {
     @GetMapping("/rooms/{id}")
     Room getRoom(@PathVariable UUID id) {
         Room room = this.roomRepository.findById(id)
-            .orElseThrow();
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
         return room;
     }
 }
