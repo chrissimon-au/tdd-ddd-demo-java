74c6230a1faea6e631e4df263b0a5044711610c9
Chris Simon <chris@devcycles.io>
2023-04-17 06:23:26 +0000

    11-GREEN: Update controller to set location header.

--- src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
+++ src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
@@ -1,14 +1,27 @@
 package au.chrissimon.universityapi.Rooms;
 
+import java.net.URI;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class RoomController {
 
     @PostMapping("/rooms")
     public ResponseEntity<Room> setupNewRoom() {
-        return ResponseEntity.created(null).body(Room.setupNew());
+        Room newRoom = Room.setupNew();
+        return ResponseEntity.created(roomUri(newRoom.getId())).body(newRoom);
+    }
+
+    URI roomUri(UUID id) {
+        return ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(id)
+            .toUri();
     }
 }
