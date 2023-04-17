7be6cb627e60ba30bd83d1b6eedc45f4b6b13478
Chris Simon <chris@devcycles.io>
2023-04-17 05:48:08 +0000

    9-REFACTOR: Start refactoring out RoomApi.

--- src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
+++ src/test/java/au/chrissimon/universityapi/Rooms/RoomTests.java
@@ -1,25 +1,20 @@
 package au.chrissimon.universityapi.Rooms;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
-import au.chrissimon.universityapi.Helpers;
-
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class RoomTests {
 
-    @Value(value="${local.server.port}")
-    private int port;
+    @Autowired
+    private RoomApi roomApi;
 
     @Test
 	public void givenIAmAnAdmin_WhenISetupANewRoom() throws Exception {
-		ResponseSpec response = Helpers.newWebClient(port)
-            .post()
-                .uri("/rooms")
-                .exchange();
+		ResponseSpec response = roomApi.setupRoom();
 
         itShouldSetupANewRoom(response);
 	}
