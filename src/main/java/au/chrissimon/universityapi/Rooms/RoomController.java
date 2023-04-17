0aaf4a98a22167aad06fe1dca14c74f2e607c0f8
Chris Simon <chris@devcycles.io>
2023-04-17 07:09:32 +0000

    13-GREEN: Simple implementation of controller method for GET room route.

--- src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
+++ src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +25,10 @@ public class RoomController {
             .path("/{id}")
             .buildAndExpand(id)
             .toUri();
+    }
+
+    @GetMapping("/rooms/{id}")
+    void getRoom() {
+
     }
 }
