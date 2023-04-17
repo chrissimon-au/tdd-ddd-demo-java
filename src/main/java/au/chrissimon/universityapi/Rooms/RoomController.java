b21a7bbb4de5a2a0ecef9f9230e6f58717e47eb3
Chris Simon <chris@devcycles.io>
2023-04-17 07:13:16 +0000

    14-GREEN: Add simple implementation of returning a hard coded room.

--- src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
+++ src/main/java/au/chrissimon/universityapi/Rooms/RoomController.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,7 +29,7 @@ public class RoomController {
     }
 
     @GetMapping("/rooms/{id}")
-    void getRoom() {
-
+    Room getRoom(@PathVariable UUID id) {
+        return new Room(id, "Test Room", 5);
     }
 }
