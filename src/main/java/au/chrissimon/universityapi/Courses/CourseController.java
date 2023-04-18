5685fb444a7758117ef41fedb382b5f36e4d71ea
Chris Simon <chris@devcycles.io>
2023-04-18 00:11:57 +0000

    19-GREEN: Implement setting of location header when including a new course in the catalog.

--- src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
+++ src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
@@ -1,14 +1,28 @@
 package au.chrissimon.universityapi.Courses;
 
+import java.net.URI;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class CourseController {
 
     @PostMapping("/courses")
     public ResponseEntity<Course> includeCourseInCatalog() {
-        return ResponseEntity.created(null).body(Course.includeInCatalog());
+        Course newCourse = Course.includeInCatalog();
+        return ResponseEntity.created(courseUri(newCourse.getId())).body(newCourse);
     }
+
+    URI courseUri(UUID id) {
+        return ServletUriComponentsBuilder
+            .fromCurrentRequest()
+            .path("/{id}")
+            .buildAndExpand(id)
+            .toUri();
+    }
+
 }
