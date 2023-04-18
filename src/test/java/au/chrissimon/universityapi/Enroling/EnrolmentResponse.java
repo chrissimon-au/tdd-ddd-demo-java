9a7d978b5a6d74b9702090a1f78866134f5550cd
Chris Simon <chris@devcycles.io>
2023-04-18 11:31:20 +0000

    27-RED: Add test to confirm new enrolment on successful enrolment.

---
+++ src/test/java/au/chrissimon/universityapi/Enroling/EnrolmentResponse.java
@@ -0,0 +1,15 @@
+package au.chrissimon.universityapi.Enroling;
+
+import java.util.UUID;
+
+public class EnrolmentResponse {
+    private UUID id;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
