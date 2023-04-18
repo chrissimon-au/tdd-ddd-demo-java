00a024049377893f4f392a2b9835eed125237f84
Chris Simon <chris@devcycles.io>
2023-04-18 01:11:49 +0000

    3-GREEN: Update controller to return location header.

--- src/main/java/au/chrissimon/universityapi/Students/StudentController.java
+++ src/main/java/au/chrissimon/universityapi/Students/StudentController.java
@@ -1,11 +1,11 @@
 package au.chrissimon.universityapi.Students;
 
-// import java.net.URI;
+import java.net.URI;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class StudentController {
@@ -14,12 +14,12 @@ public class StudentController {
     ResponseEntity<Student> registerNewStudent() {
         Student newStudent = Student.register();
 
-        // URI newStudentLocation = ServletUriComponentsBuilder
-        //                 .fromCurrentRequest()
-        //                 .path("/{id}")
-        //                 .buildAndExpand(newStudent.getId())
-        //                 .toUri();
+        URI newStudentLocation = ServletUriComponentsBuilder
+                        .fromCurrentRequest()
+                        .path("/{id}")
+                        .buildAndExpand(newStudent.getId())
+                        .toUri();
 
-        return ResponseEntity.created(null).body(newStudent);
+        return ResponseEntity.created(newStudentLocation).body(newStudent);
     }
 }
