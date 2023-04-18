d53b62b10c063d6add9ed6f8309fe8a58e50e412
Chris Simon <chris@devcycles.io>
2023-04-18 06:32:41 +0000

    24-GREEN: Check for room Id before including a new course in the catalog.

--- src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
+++ src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
@@ -3,29 +3,38 @@ package au.chrissimon.universityapi.Courses;
 import java.net.URI;
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import au.chrissimon.universityapi.Rooms.Room;
+import au.chrissimon.universityapi.Rooms.RoomRepository;
 import jakarta.validation.Valid;
 
 @RestController
 public class CourseController {
 
     private CourseRepository courseRepository;
+    private RoomRepository roomRepository;
 
-    public CourseController(CourseRepository courseRepository) {
+    public CourseController(CourseRepository courseRepository, RoomRepository roomRepository) {
         super();
         this.courseRepository = courseRepository;
+        this.roomRepository = roomRepository;
     }
 
     @PostMapping("/courses")
     public ResponseEntity<Course> includeCourseInCatalog(@Valid @RequestBody Course courseRequest) {
-        Course newCourse = Course.includeInCatalog(courseRequest.getName(), courseRequest.getRoomId());
+        Room room = roomRepository.findById(courseRequest.getRoomId())
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST));
+
+        Course newCourse = Course.includeInCatalog(courseRequest.getName(), room.getId());
         courseRepository.save(newCourse);
         return ResponseEntity.created(courseUri(newCourse.getId())).body(newCourse);
     }
