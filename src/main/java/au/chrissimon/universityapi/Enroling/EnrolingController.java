acc63b7c85faa0dbf09adafb2d97d48bbed664a6
Chris Simon <chris@devcycles.io>
2023-04-18 12:03:10 +0000

    29-GREEN: Pass the courseId back in the enrolment.

--- src/main/java/au/chrissimon/universityapi/Enroling/EnrolingController.java
+++ src/main/java/au/chrissimon/universityapi/Enroling/EnrolingController.java
@@ -5,14 +5,15 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EnrolingController {
     
     @PostMapping("/students/{studentId}/courses")
-    public ResponseEntity<Enrolment> enrolStudentInCourse(@PathVariable UUID studentId)
+    public ResponseEntity<Enrolment> enrolStudentInCourse(@PathVariable UUID studentId, @RequestBody Enrolment enrolment)
     {
-        return ResponseEntity.created(null).body(new Enrolment(UUID.randomUUID(), studentId));
+        return ResponseEntity.created(null).body(new Enrolment(UUID.randomUUID(), studentId, enrolment.getCourseId()));
     }
 }
