9e3af5874cb1a4c3da6a83f98277738540c201c2
Chris Simon <chris@devcycles.io>
2023-04-18 06:28:36 +0000

    23-REFACTOR: refactor to use spring boot validation to test for null roomId.

--- src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
+++ src/main/java/au/chrissimon/universityapi/Courses/CourseController.java
@@ -3,15 +3,15 @@ package au.chrissimon.universityapi.Courses;
 import java.net.URI;
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class CourseController {
@@ -24,10 +24,7 @@ public class CourseController {
     }
 
     @PostMapping("/courses")
-    public ResponseEntity<Course> includeCourseInCatalog(@RequestBody Course courseRequest) {
-        if (courseRequest.getRoomId() == null) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Course> includeCourseInCatalog(@Valid @RequestBody Course courseRequest) {
         Course newCourse = Course.includeInCatalog(courseRequest.getName(), courseRequest.getRoomId());
         courseRepository.save(newCourse);
         return ResponseEntity.created(courseUri(newCourse.getId())).body(newCourse);
