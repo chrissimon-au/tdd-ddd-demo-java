18fc12fc10d9e1fc507534e3c46c8166a47c588b
Chris Simon <chris@devcycles.io>
2023-04-19 04:02:45 +0000

    34-RED: interim refactoring stage illustrating ideal expression of ubuiqitous language and highlight difficulties with current design.

--- src/main/java/au/chrissimon/universityapi/Courses/Course.java
+++ src/main/java/au/chrissimon/universityapi/Courses/Course.java
@@ -3,13 +3,15 @@ package au.chrissimon.universityapi.Courses;
 import java.util.Optional;
 import java.util.UUID;
 
+import au.chrissimon.universityapi.Enroling.Enrolment;
 import au.chrissimon.universityapi.Rooms.Room;
+import au.chrissimon.universityapi.Students.Student;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotNull;
 
 @Entity
-class Course {
+public class Course {
     private @Id UUID id;
     private String name;
     @NotNull
@@ -53,5 +55,10 @@ class Course {
 
     public void setRoomId(UUID roomId) {
         this.roomId = roomId;
+    }
+
+    public Enrolment Enrol(Student student) {
+        // Unable to implement rules as we don't have access to the room capacity, or the current number of enrolments.
+        return null;
     }
 }
