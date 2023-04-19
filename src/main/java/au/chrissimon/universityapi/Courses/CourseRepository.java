170b5f16a7f3a72aa53ea1f2103f33a1216cbd03
Chris Simon <chris@devcycles.io>
2023-04-19 04:04:01 +0000

    Revert "34-RED: interim refactoring stage illustrating ideal expression of ubuiqitous language and highlight difficulties with current design."
    This reverts commit b1ca7e3e75113b98e64357717b22761ef654d6a8.

--- src/main/java/au/chrissimon/universityapi/Courses/CourseRepository.java
+++ src/main/java/au/chrissimon/universityapi/Courses/CourseRepository.java
@@ -4,6 +4,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseRepository extends JpaRepository<Course, UUID> {
+interface CourseRepository extends JpaRepository<Course, UUID> {
 
 }
