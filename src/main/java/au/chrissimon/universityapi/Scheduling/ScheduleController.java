fb45348137eb87f204d355a36db475c12be96597
Chris Simon <chris@devcycles.io>
2023-04-19 10:29:52 +0000

    38-GREEN: Naive implementation to verify response structure.

--- src/main/java/au/chrissimon/universityapi/Scheduling/ScheduleController.java
+++ src/main/java/au/chrissimon/universityapi/Scheduling/ScheduleController.java
@@ -1,14 +1,48 @@
 package au.chrissimon.universityapi.Scheduling;
 
+import java.util.Set;
+
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import au.chrissimon.universityapi.Courses.Course;
+import au.chrissimon.universityapi.Courses.CourseRepository;
+import au.chrissimon.universityapi.Rooms.Room;
+import au.chrissimon.universityapi.Rooms.RoomRepository;
+
 @RestController
 public class ScheduleController {
-    
+    private CourseRepository courseRepository;
+    private RoomRepository roomRepository;
+
+    public ScheduleController(CourseRepository courseRepository,
+        RoomRepository roomRepository) {
+        super();
+        this.courseRepository = courseRepository;
+        this.roomRepository = roomRepository;
+    }
+
     @PostMapping("/schedules")
-    public ResponseEntity<String> schedule() {
-        return ResponseEntity.ok().body("");
+    public ResponseEntity<Schedule> schedule() {
+        Course probeCourse = new Course();
+        probeCourse.setName("Scheduling Test Course");
+        ExampleMatcher courseMatcher = ExampleMatcher.matching().withIgnorePaths("id", "roomId");
+        Course course = courseRepository.findOne(Example.of(probeCourse, courseMatcher)).get();
+        
+        Room probeRoom = new Room();
+        ExampleMatcher roomMatcher = ExampleMatcher.matching().withIgnorePaths("id", "capacity");
+        probeRoom.setName("Scheduling Test Room");
+        Room room = roomRepository.findOne(Example.of(probeRoom, roomMatcher)).get();
+
+        course.setRoomId(room.getId());
+
+        Schedule schedule = new Schedule();
+        Set<Course> scheduledCourses = Set.of(course);
+        schedule.setScheduledCourses(scheduledCourses);
+
+        return ResponseEntity.ok().body(schedule);
     }
 }
