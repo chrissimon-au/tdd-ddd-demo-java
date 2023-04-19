af6156bb38aa52c0aa9c2ebc0a886cd2c4d22fc0
Chris Simon <chris@devcycles.io>
2023-04-19 10:17:28 +0000

    38-RED: Add test for the contents of the schedule response

--- src/test/java/au/chrissimon/universityapi/Scheduling/SchedulingTests.java
+++ src/test/java/au/chrissimon/universityapi/Scheduling/SchedulingTests.java
@@ -6,23 +6,70 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.reactive.server.WebTestClient.ResponseSpec;
 
+import au.chrissimon.universityapi.Courses.CourseApi;
+import au.chrissimon.universityapi.Courses.CourseResponse;
+import au.chrissimon.universityapi.Courses.IncludeCourseRequest;
+import au.chrissimon.universityapi.Enroling.EnrolmentApi;
+import au.chrissimon.universityapi.Rooms.RoomApi;
+import au.chrissimon.universityapi.Rooms.RoomResponse;
+import au.chrissimon.universityapi.Rooms.SetupRoomRequest;
+import au.chrissimon.universityapi.Students.RegisterStudentRequest;
+import au.chrissimon.universityapi.Students.StudentApi;
+import au.chrissimon.universityapi.Students.StudentResponse;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class SchedulingTests {
 
     @Autowired
-    ScheduleApi scheduleApi;
+    private ScheduleApi scheduleApi;
+
+    @Autowired
+    private RoomApi roomApi;
+
+    @Autowired
+    private StudentApi studentApi;
+
+    @Autowired
+    private CourseApi courseApi;
+
+    @Autowired
+    private EnrolmentApi enrolmentApi;
 
     @Test
     public void givenIAmAnAdmin_WhenIScheduleCourses() {
+        StudentResponse student = studentApi.getStudentFromResponse(studentApi.registerStudent(new RegisterStudentRequest("Test Student")));
+
+        CourseResponse course = courseApi.getCourseFromResponse(courseApi.includeNewCourseInCatalog(new IncludeCourseRequest("Scheduling Test Course")));
+
+        enrolmentApi.enrolStudentInCourse(student, course);
+
+        RoomResponse room = roomApi.getRoomFromResponse(roomApi.setupRoom(new SetupRoomRequest("Scheduling Test Room", 3)));
+
         ResponseSpec response = scheduleApi.schedule();
 
         itShouldScheduleTheCourses(response);
+
+        ScheduleResponse schedule = scheduleApi.getScheduleFromResponse(response);
+
+        itShouldListTheScheduledCourses(schedule, course, room);
     }
 
     private void itShouldScheduleTheCourses(ResponseSpec response) {
         response
             .expectStatus()
             .isOk();
+    }
+
+    private void itShouldListTheScheduledCourses(ScheduleResponse response, CourseResponse course, RoomResponse room) {
+        assertThat(response.scheduledCourses).hasSize(1);
+
+        response.scheduledCourses.forEach(scheduledCourse -> {
+            assertThat(scheduledCourse.getId()).isEqualTo(course.getId());
+            assertThat(scheduledCourse.getName()).isEqualTo(course.getName());
+            assertThat(scheduledCourse.getRoomId()).isEqualTo(room.getId());
+        });
     }
 
 }
