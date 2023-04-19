c9badbcf7564ae66f34ac86ca73d2980b8edf004
Chris Simon <chris@devcycles.io>
2023-04-19 00:09:03 +0000

    30-GREEN: Add implementation that checks for student Id before attempting to enrol.

--- src/main/java/au/chrissimon/universityapi/Enroling/EnrolingController.java
+++ src/main/java/au/chrissimon/universityapi/Enroling/EnrolingController.java
@@ -2,18 +2,32 @@ package au.chrissimon.universityapi.Enroling;
 
 import java.util.UUID;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import au.chrissimon.universityapi.Students.StudentRepository;
 
 @RestController
 public class EnrolingController {
     
+    private StudentRepository studentRepository;
+
+    public EnrolingController(StudentRepository studentRepository) {
+        super();
+        this.studentRepository = studentRepository;
+    }
+
     @PostMapping("/students/{studentId}/courses")
     public ResponseEntity<Enrolment> enrolStudentInCourse(@PathVariable UUID studentId, @RequestBody Enrolment enrolment)
     {
+        studentRepository.findById(studentId)
+            .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+
         return ResponseEntity.created(null).body(new Enrolment(UUID.randomUUID(), studentId, enrolment.getCourseId()));
     }
 }
