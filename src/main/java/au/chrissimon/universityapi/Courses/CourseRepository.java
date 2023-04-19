3b6e2a6b6e324a26bd0545fb33461aace7b9979c
Chris Simon <chris@devcycles.io>
2023-04-19 00:14:36 +0000

    31-GREEN: Add check for course id before enroling.

--- src/main/java/au/chrissimon/universityapi/Courses/CourseRepository.java
+++ src/main/java/au/chrissimon/universityapi/Courses/CourseRepository.java
@@ -4,6 +4,6 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-interface CourseRepository extends JpaRepository<Course, UUID> {
+public interface CourseRepository extends JpaRepository<Course, UUID> {
     
 }
