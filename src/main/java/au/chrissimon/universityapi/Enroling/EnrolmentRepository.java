78414b8990640a66713c712d2a04fbd2ffea0cc1
Chris Simon <chris@devcycles.io>
2023-04-19 01:03:20 +0000

    34-GREEN: Naive implementation for failing enrolment if the course is full.

--- src/main/java/au/chrissimon/universityapi/Enroling/EnrolmentRepository.java
+++ src/main/java/au/chrissimon/universityapi/Enroling/EnrolmentRepository.java
@@ -3,7 +3,8 @@ package au.chrissimon.universityapi.Enroling;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-interface EnrolmentRepository extends JpaRepository<Enrolment, UUID> {
+interface EnrolmentRepository extends JpaRepository<Enrolment, UUID>, JpaSpecificationExecutor<Enrolment> {
     
 }
