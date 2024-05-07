5addb012328714dc873af2fe42491ffc00105579
Chris Simon <chris@devcycles.io>
2024-05-07 14:28:52 +0100

    34a-RED: Add concurrency test that confirms that the course capacity isn't respected when concurrent requests arrive at the API.

--- src/test/java/au/chrissimon/universityapi/Enroling/EnrolmentApi.java
+++ src/test/java/au/chrissimon/universityapi/Enroling/EnrolmentApi.java
@@ -2,6 +2,7 @@ package au.chrissimon.universityapi.Enroling;
 
 import java.net.URI;
 import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
@@ -28,6 +29,10 @@ public class EnrolmentApi {
                 .uri("/students/" + student.getId() + "/courses")
                 .bodyValue(enrolStudentInCourseRequest)
             .exchange();
+    }
+
+    public CompletableFuture<ResponseSpec> enrolStudentInCourseAsync(StudentResponse student, CourseResponse course) {
+        return CompletableFuture.supplyAsync(() -> enrolStudentInCourse(student, course));
     }
 
     public EnrolmentResponse getEnrolmentFromResponse(ResponseSpec enrolmentResponse) {
